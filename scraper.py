900204be6235404f6d4216ab5413a724bb9cedde
AdarsHH30 <adarshhegde380@gmail.com>
2025-05-10 22:32:37 +0530

    changed the op format to json

--- scraper.py
+++ scraper.py
@@ -1,9 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+from colorama import init, Fore, Style
 
-print("Enter the Job that you need to find :")
-job_name = input("")
+print(f"Enter the Job that you need to find :")
+job_name = input(f"{Fore.GREEN}>>  {Style.RESET_ALL}\n")
+init()
+
+print(
+    f"Enter the Number of Job pages that you need to find ",
+    end="",
+)
+print(f"{Fore.RED}( Note : 1 page contains 25 jobs ){Style.RESET_ALL}:")
+try:
+    job_count = int(input(f"{Fore.GREEN}>>  {Style.RESET_ALL}"))
+except Exception as e:
+    print(f"{Fore.RED}Please enter a valid number{Style.RESET_ALL}")
+    exit(0)
+print(f"\nSearching for Jobs...")
+json_file = open(f"Jobs/{job_name}_jobs.json", "w")
+json_file.write("[")
 page_no = 1
 web_page = requests.get(
     f"https://www.timesjobs.com/candidate/job-search.html?from=submit&luceneResultSize=25&txtKeywords={job_name}&postWeek=60&searchType=personalizedSearch&actualTxtKeywords=python&searchBy=0&rdoOperator=OR&pDate=I&sequence={page_no}&startPage=1"
@@ -19,9 +35,11 @@ count = 0
 
 def find_job():
     global count
-    print("--------------------------------------------------------")
+
     for jobs in jobs_:
-        print(f"Job Number : {count + 1}")
+        if count:
+            json_file.write(",")
+        # print(f"Job Number : {count + 1}")
         count += 1
         job_header = jobs.header.div
         job_title = job_header.h2.text
@@ -32,28 +50,25 @@ def find_job():
         skill_required = jobs.find("ul", class_="list-job-dtl clearfix").div.find_all(
             "span"
         )
-        place = jobs.find("li", class_="srp-zindex location-tru").text.strip("")
-
-        print(f"Job Title : {job_title}")
-        print(f"Job Link : {job_link}")
-        print(f"Job Description : {job_description}")
-        print(f"Skill Required is :", end=" ")
-        print(",".join([skill.text.strip() for skill in skill_required]))
-        print("\n\n")
-    print("--------------------------------------------------------")
-
-
-# print(f"Skill Required : {skill}")
-# print(f"Skill Required : {skill}")
-# print(f"Location : {place}")
-# print("--------------------------------------------------------")
+        skill_required = [
+            skill.text.strip() for skill in skill_required if skill.text.strip()
+        ]
+        place = jobs.find("li", class_="srp-zindex location-tru").text.strip()
+        json_file.write("{")
+        json_file.write(f'"Job Title" : "{job_title}",')
+        json_file.write(f'"Job Link" : "{job_link}",')
+        json_file.write(f'"Job Description" : "{job_description}",')
+        json_file.write(f'"Skill Required" : "{",".join(skill_required)}",')
+        json_file.write(f'"Location" : "{"".join(place.split())}"')
+        json_file.write("}")
+        json_file.write("\n\n")
 
 
 while True:
-    if count >= 3000:
-        # print("3000 Jobs Found")
+    if job_count < page_no:
         break
     try:
+
         web_page = requests.get(
             f"https://www.timesjobs.com/candidate/job-search.html?from=submit&luceneResultSize=25&txtKeywords={job_name}&postWeek=60&searchType=personalizedSearch&actualTxtKeywords=python&searchBy=0&rdoOperator=OR&pDate=I&sequence={page_no}&startPage=1"
         ).text
@@ -69,6 +84,12 @@ while True:
 
 end_time = time.time()
 
-print(f"Total Time Taken : {end_time - start_time} seconds")
-print(f"Total Jobs Found : {count}")
+json_file.write("]")
+json_file.close()
+print(f"\n\n{Fore.GREEN}Job Search Completed{Style.RESET_ALL}\n\n")
+print(
+    f"Total Time Taken : {Fore.GREEN}{end_time - start_time} seconds {Style.RESET_ALL}"
+)
+print(f"Total Jobs Found :{Fore.GREEN} {count} {Style.RESET_ALL}")
+print(f"You can find the jobs in {Fore.GREEN}{job_name}_jobs.json{Style.RESET_ALL}")
 print("--------------------------------------------------------")
